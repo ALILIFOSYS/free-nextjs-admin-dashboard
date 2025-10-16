78b7de153e0914414a20118da2b59c54807c2f2b
Ali akbar <aliakbar@lifosys.com>
2025-10-16 12:46:31 +0530

    update base url

--- src/constents/serverBaseUrl.js
+++ src/constents/serverBaseUrl.js
@@ -1,2 +1,2 @@
-export const BaseUrl = 'http://localhost:3001/admin/api'; 
-// export const BaseUrl = 'https://admin-server-ten.vercel.app/admin/api'; 
+// export const BaseUrl = 'http://localhost:3001/admin/api'; 
+export const BaseUrl = 'http://aiilab-server-env.eba-q5rfdznc.ap-south-1.elasticbeanstalk.com/admin/api'; 
